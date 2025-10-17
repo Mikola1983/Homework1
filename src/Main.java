e19e5ac82b149d41bcbb51ea627c4893a82370ad
Mikola1983 <Mikolaprigorsk@mail.ru>
2025-10-17 20:16:37 +0700

    Массивы 1 задание

--- src/Main.java
+++ src/Main.java
@@ -4,93 +4,96 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Циклы, урок 2. Домашнее задание №7.");
+        System.out.println("Массивы, урок 1. Домашнее задание №8.");
         // Задание 1.
-        byte firstFriday = 2;
-        for (byte day = 1; day <= 31; day++) {
-            if (day < firstFriday) { // Отсечка начальных дней месяца до первой пятницы.
-                continue;
-            }
-            if ((day - firstFriday) % 7 == 0) { // Проверка является ли день пятницей.
-                System.out.println("Сегодня пятница, " + day + " число. Необходимо подготовить отчет.");
-            }
+        int mass[] = new int[3];
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = i + 1;
         }
-        // Задание 2, 1 вариант.
-        int distance = 0;
-        do {
-            System.out.println("Держитесь! осталось " + (42195 - distance) + " метров.");
-            distance = distance + 500;
+        double massiv[] = {1.57, 7.654, 9.986};
+        for (int i = 0; i < massiv.length; i++) {
         }
-        while (distance <= 42195);
-        // Задание 2, 2 вариант.
-        distance = 0;
-        for (; distance <= 42195; distance = distance + 500) {
-            System.out.println("Держитесь! осталось " + (42195 - distance) + " метров.");
+        int massive[] = new int[12];
+        for (int i = 0; i < massive.length; i++) {
+            massive[i] = i * i;
         }
-        //Задание 3, 1 вариант
-        int tax = 100; // Тариф за 1 день
-        int pay = 1000; // Внесенная сумма
-        int days = 1; // Счетчик дней
-        if (pay < tax) { // Проверка на количество внесенных средств
-            System.out.println("Недостаточная сумма");
-            days = 0;
-        }
-        while (pay > tax) {
-            days++;
-            if (days % 5 == 0) {
-                continue;
-            }
-            pay = pay - tax;
-        }
-        System.out.println("Внесенная сумма достаточна для " + days + " дней стоянки ТС.");
-        // Задание 3, 2 вариант
-        days = 1;
-        pay = 1000;
-        if (pay < tax) { // Проверка на количество внесенных средств
-            System.out.println("Недостаточная сумма");
-            days = 0;
-        }
-        for (; pay > tax; ) {
-            days++;
-            if (days % 5 == 0) {
-                continue;
-            }
-            pay = pay - tax;
-        }
-        System.out.println("Внесенная сумма достаточна для " + days + " дней стоянки ТС.");
-        // Задание 4.
-        int month = 0;
-        double total = 0;
-        while (true) {
-            total = total + 15000;
-            month++;
-            if (month % 6 == 0) {
-                total = total + total * 0.07;
-            }
-            System.out.printf("Сумма на счету %.2f рублей. Месяц %d", total, month);
-            System.out.println();
-            if (total >= 12000000) {
+        // Задание 2.
+        for (int i = 0; i < mass.length; i++) {
+            if (i == mass.length - 1) {
+                System.out.println(mass[i]);
                 break;
             }
+            System.out.print(mass[i] + ", ");
         }
-        // Задание 5.
-        int charge = 20;
-        int minute = 0;
-        int overheats = 0;
-        while (charge < 100 && overheats <= 3) {
-            System.out.println("заряд" + charge + "время" + minute);
-            if (minute % 10 == 0) {
-                overheats++;
-                minute = minute + 2;
-                continue;
-            }
-            if (overheats == 3) {
-                System.out.println("Зарядка прекращена. Текущий заряд " + charge + "%");
+        for (int i = 0; i < massiv.length; i++) {
+            if (i == massiv.length - 1) {
+                System.out.println(massiv[i]);
                 break;
             }
-            minute++;
-            charge = charge + 2;
+            System.out.print(massiv[i] + ", ");
         }
-        System.out.println("Время зарядки составило " + minute + " минут.");
+        for (int i = 0; i < massive.length; i++) {
+            if (i == massive.length - 1) {
+                System.out.println(massive[i]);
+                break;
+            }
+            System.out.print(massive[i] + ", ");
+        }
+        //Задание 3.
+        int temp;
+        double tempD;
+        for (int i = 0; i < mass.length / 2; i++) {
+            temp = mass[i];
+            mass[i] = mass[mass.length - 1 - i];
+            mass[mass.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < mass.length; i++) {
+            if (i == mass.length - 1) {
+                System.out.print(mass[i]);
+                break;
+            }
+            System.out.print(mass[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 0; i < massiv.length / 2; i++) {
+            tempD = massiv[i];
+            massiv[i] = massiv[massiv.length - 1 - i];
+            massiv[massiv.length - 1 - i] = tempD;
+        }
+        for (int i = 0; i < massiv.length; i++) {
+            if (i == massiv.length - 1) {
+                System.out.print(massiv[i]);
+                break;
+            }
+            System.out.print(massiv[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 0; i < massive.length / 2; i++) {
+            temp = massive[i];
+            massive[i] = massive[massive.length - 1 - i];
+            massive[massive.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < massive.length; i++) {
+            if (i == massive.length - 1) {
+                System.out.print(massive[i]);
+                break;
+            }
+            System.out.print(massive[i] + ", ");
+        }
+        System.out.println();
+        //Задание 4.
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] % 2 != 0) {
+                mass[i] = mass[i] + 1;
+            }
+            System.out.print(mass[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < massive.length; i++) {
+            if (massive[i] % 2 != 0) {
+                massive[i] = massive[i] + 1;
+            }
+            System.out.print(massive[i] + " ");
+        }
     }
 }
