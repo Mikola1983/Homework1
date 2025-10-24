257efcce338699a28a228bd4239c337a72fe1a7f
Mikola1983 <Mikolaprigorsk@mail.ru>
2025-10-25 00:54:54 +0700

    Методы, домашнее задание. Исправлено.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Locale;
 
@@ -8,9 +9,9 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Методы. Домашнее задание 10.");
-        int currentYear = 2021;
+        int currentYear = LocalDate.now().getYear();
         int deviceOperationalSistem = 1;
-        int deviceYear = 2020;
+        int deviceYear = 2024;
         int distance = 15;
         System.out.println("Задание1.");
         detectYear(currentYear);
@@ -24,24 +25,28 @@ public class Main {
 
     // Задание 1
     public static void detectYear(int year) {
-        int firstYear = 1584;
-        if ((year - firstYear) % 4 == 0 && (year - firstYear) % 100 != 0 && (year - firstYear) % 400 == 0) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является високосным.");
-        } else if ((year - firstYear) % 4 != 0 || (year - firstYear) % 100 == 0 && (year - firstYear) % 400 != 0) {
+        } else {
             System.out.println(year + " год не является високосным.");
-        } else System.out.println(year + " год является високосным.");
+        }
+        if (year < 1584) {
+            System.out.println(year + " год не является високосным.");
+        }
     }
 
     // Задание 2.
     public static void detectOS(int clientDeviceYear, int deviceOS) {
-        if (deviceOS == 0 && clientDeviceYear >= 2015) {
+        int currentYear = LocalDate.now().getYear();
+        System.out.println(currentYear);
+        if (deviceOS == 0 && clientDeviceYear >= currentYear) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (deviceOS == 0 && clientDeviceYear < 2015) {
+        } else if (deviceOS == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
-        if (deviceOS == 1 && clientDeviceYear >= 2015) {
+        if (deviceOS == 1 && clientDeviceYear >= currentYear) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (deviceOS == 1 && clientDeviceYear < 2015) {
+        } else if (deviceOS == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
 
@@ -50,7 +55,7 @@ public class Main {
     // Задание 3.
     public static int deliveryTime(int deliveryDistance) {
         int deliveryTime = 0;
-        if (deliveryDistance <= 20) {
+        if (deliveryDistance < 20) {
             deliveryTime = 1;
         } else if (deliveryDistance < 60) {
             deliveryTime = 2;
@@ -65,6 +70,8 @@ public class Main {
     public static void printDays(int days) {
         if (days <= 3) {
             System.out.println("Потртебуется дней: " + days + " для доставки");
-        } else System.out.println("Доставки свыше 100 км. нет");
+        } else {
+            System.out.println("Доставки свыше 100 км. нет");
+        }
     }
 }
