6210ed83ec5c275bd21bb981a833d7ad668eab86
Mikola1983 <Mikolaprigorsk@mail.ru>
2025-10-28 23:15:16 +0700

    Методы класса Object

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Объекты. Домашнее задание 11.");
-        Author levTolstoy = new Author("Лев", "Толстой");
-        Author fedorDostoevski = new Author("Федор", "Достоевский");
-        Book warAndPeace = new Book(levTolstoy, "Война и мир", 2001);
-        System.out.println(warAndPeace.getAuthor() + " " + warAndPeace.getTitle() + " " + warAndPeace.getYear());
-        Book besy = new Book(fedorDostoevski, "Бесы", 1995);
-        System.out.println(besy.getAuthor() + " " + besy.getTitle() + " " + besy.getYear());
-        besy.setYear(2000);
-        System.out.println(besy.getAuthor() + " " + besy.getTitle() + " " + besy.getYear());
+
     }
 }
